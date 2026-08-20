3209881028d8aae7323bb94f9ff6f440bcf1c647
PrachiGupta1509 <prachiguptashivprasad@gmail.com>
2026-08-20 11:09:37 +0530

    Fix prediction indexing for wine type prediction

--- wine_type_deployment.py
+++ wine_type_deployment.py
@@ -46,7 +46,7 @@ df = pd.DataFrame({
 if st.button("Predict Wine Type"):
     prediction = model.predict(df)[0]
 
-    if prediction == 0:
+    if prediction[0] == 0:
         st.write("The predicted wine type is Red Wine.")
     else:
         st.write("The predicted wine type is White Wine.")
